1f1eac26e2dde31a754a9bc4646763b3048eb2ef
TheLongOne24 <99805384+TheLongOne24@users.noreply.github.com>
2023-04-03 17:33:59 +0300

    reservation and client done

--- HotelReservationManager/HotelReservationManager/Models/Reservation.cs
+++ HotelReservationManager/HotelReservationManager/Models/Reservation.cs
@@ -3,16 +3,14 @@
     public class Reservation
     {
         public int ResId { get; set; }
-
         public int RoomsId { get; set; }
-
         public string Usename { get; set; }
-        public virtual HashSet<Clients> Clients { get; set; }
         public DateTime Arrival_date { get; set; }
         public DateTime Departure_date { get; set; }
         public bool Breakfast { get; set; }
         public bool All_inclusive { get; set; }
         public double Price { get; set; }
+        public virtual HashSet<Clients> Clients { get; set; }
 
         public Reservation()
         {
